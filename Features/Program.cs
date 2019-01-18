5b500965d573ebacf75b3f0f1bc70951ef9ed6b4
alex.paclin <alex.paclin@gmail.com>
2019-01-18 13:42:17 +0500

    foreach

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Features
 {
@@ -10,6 +7,35 @@ namespace Features
     {
         static void Main(string[] args)
         {
+            var developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott" },
+                new Employee { Id = 2, Name = "Chris" }
+            };
+
+            var sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
+
+            foreach (var person in developers)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            foreach (var person in sales)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
